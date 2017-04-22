2ffd74110c20ba6dc9fe95446d263e7f6b99e8e0
ZviZdo <zvizdo@gmail.com>
2017-04-22 15:57:28 +0200

    Bouncy Castle research

--- src/MadWare.Passbook/PassSigner/BouncyCastlePassSiger.cs
+++ src/MadWare.Passbook/PassSigner/BouncyCastlePassSiger.cs
@@ -23,7 +23,7 @@ namespace MadWare.Passbook.PassSigner
             Org.BouncyCastle.X509.X509Certificate apple = new X509CertificateParser().ReadCertificate(appleCert.RawData);
             Org.BouncyCastle.X509.X509Certificate pass = new X509CertificateParser().ReadCertificate(passCert.RawData);
 
-            var privateKey = this.GetRsaKeyPair(passCert.GetRSAPrivateKey().ExportParameters(true)).Private;
+            var privateKey = this.GetPrivateKey(passCert.GetRSAPrivateKey().ExportParameters(true));
             var generator = new CmsSignedDataGenerator();
             generator.AddSigner(privateKey, pass, CmsSignedGenerator.DigestSha1);
 
@@ -44,31 +44,47 @@ namespace MadWare.Passbook.PassSigner
             throw new NotImplementedException();
         }
 
-        private AsymmetricCipherKeyPair GetRsaKeyPair(
+        private RsaPrivateCrtKeyParameters GetPrivateKey(
           RSAParameters rp)
         {
-            BigInteger modulus = new BigInteger(1, rp.Modulus);
-            BigInteger pubExp = new BigInteger(1, rp.Exponent);
-
-            RsaKeyParameters pubKey = new RsaKeyParameters(
-                false,
-                modulus,
-                pubExp);
-
-            RsaPrivateCrtKeyParameters privKey = new RsaPrivateCrtKeyParameters(
-                modulus,
-                pubExp,
+            return new RsaPrivateCrtKeyParameters(
+                new BigInteger(1, rp.Modulus),
+                new BigInteger(1, rp.Exponent),
                 new BigInteger(1, rp.D),
                 new BigInteger(1, rp.P),
                 new BigInteger(1, rp.Q),
                 new BigInteger(1, rp.DP),
                 new BigInteger(1, rp.DQ),
                 new BigInteger(1, rp.InverseQ));
+        }
 
-            return new AsymmetricCipherKeyPair(pubKey, privKey);
+        public static RSAParameters ToRSAParameters(RsaPrivateCrtKeyParameters privKey)
+        {
+            RSAParameters rp = new RSAParameters();
+            rp.Modulus = privKey.Modulus.ToByteArrayUnsigned();
+            rp.Exponent = privKey.PublicExponent.ToByteArrayUnsigned();
+            rp.P = privKey.P.ToByteArrayUnsigned();
+            rp.Q = privKey.Q.ToByteArrayUnsigned();
+            rp.D = ConvertRSAParametersField(privKey.Exponent, rp.Modulus.Length);
+            rp.DP = ConvertRSAParametersField(privKey.DP, rp.P.Length);
+            rp.DQ = ConvertRSAParametersField(privKey.DQ, rp.Q.Length);
+            rp.InverseQ = ConvertRSAParametersField(privKey.QInv, rp.Q.Length);
+            return rp;
+        }
+
+        private static byte[] ConvertRSAParametersField(BigInteger n, int size)
+        {
+            byte[] bs = n.ToByteArrayUnsigned();
+            if (bs.Length == size)
+                return bs;
+            if (bs.Length > size)
+                throw new ArgumentException("Specified size too small", "size");
+            byte[] padded = new byte[size];
+            Array.Copy(bs, 0, padded, size - bs.Length, bs.Length);
+            return padded;
         }
 
     }
 }
 
-# endif
+#endif
